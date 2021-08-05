315e53634fc798895e6a2fb1b4b2c69840d293c5
tande <tande1124@163.com>
2021-08-05 18:26:36 +0800

    'css3-pratice'

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,19 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <div class="content layout">
+			<layout1></layout1>
+		</div>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import Layout1 from "@/components/layout/使用clip-path排版蜂窝布局.vue";
 
 export default defineComponent({
   name: 'Home',
   components: {
-    HelloWorld,
+    Layout1,
   },
 });
 </script>
